c4464484f9c6b73b58aa7c83cca7b82182653536
Jeff Hazel <jeff.hazel@getg5.com>
2017-09-16 21:17:21 -0700

    HW0

---
+++ HW0/hw0.py
@@ -0,0 +1,11 @@
+################################
+# CS 261 - Fall 2017
+# Homework 0
+# Name: [Fill in your name here]
+################################
+
+def hello_world(val=''):
+
+    #TODO: print "Hello, World" followed by the OPTIONAL string passed in via the val param
+
+    return 0;
